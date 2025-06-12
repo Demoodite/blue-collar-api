9594036a90a8956da55ab180b5a006317504be25
Demoodite <m.jafari1379@gmail.com>
2025-06-12 20:48:28 +0330

    Added all needed models

--- db/models.py
+++ db/models.py
@@ -1,12 +1,29 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, create_engine
 
 
-class User(SQLModel, table = True):
+class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     username: str
     password: str
 
-class Employee(SQLModel, table = True):
-    user_id: int
-    name: str
 
+class Token(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    token: str
+    user_id: int = Field(foreign_key="user.id")
+
+
+class Employee(SQLModel, table=True):
+    user_id: int = Field(foreign_key="user.id")
+    name: str
+    title: str
+    current_task: str
+
+
+class Entrance(SQLModel, table=True):
+    user_id: int = Field(foreign_key="user.id")
+    enterTimestamp: int
+    leaveTimestamp: int
+
+
+engine = create_engine("sqlite:///bcollar.db")
